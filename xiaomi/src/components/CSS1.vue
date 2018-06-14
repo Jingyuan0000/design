a916ce551b8cf518c5e4d79b9da87155427641d5
Jill <jill.li.0822@gmail.com>
2018-06-14 23:57:02 +0800

    改了字体

--- xiaomi/src/components/CSS1.vue
+++ xiaomi/src/components/CSS1.vue
@@ -12,52 +12,71 @@
           <img src="https://i1.mifile.cn/a4/xmad_15123939053142_dFlAw.jpg" alt="">
         </div>
       </div>
-      <div class="ads-right">
+      <div class="ads-right" clearfix>
         <ul>
           <li>
-            <a href="#">
-              <img src="https://i1.mifile.cn/a1/pms_1524883847.49276938!220x220.jpg" alt="">
-              <strong></strong>
-              <p></p>
-              <span></span>
+            <a href="https://www.mi.com/mitv4A/43y/">
+              <img src="https://i1.mifile.cn/a1/pms_1503909300.25797209!220x220.png" alt="">
+              <strong> 小米电视4A 43英寸青春版1</strong>
+              <p>全高清屏/人工智能语音</p>
+              <span>1399</span>
+              <dl>1699</dl>
            </a>
           </li>
           <li>
-            <img src="https://i1.mifile.cn/a1/pms_1522318330.86967810!220x220.jpg" alt="">
-            <strong></strong>
-            <p></p>
-            <span></span>
+            <a href="https://www.mi.com/mitv4A/43y/">
+              <img src="https://i1.mifile.cn/a1/pms_1510111588.69169839!220x220.jpg" alt="">
+              <strong> 小米电视4A 43英寸青春版2</strong>
+              <p>全高清屏/人工智能语音</p>
+              <span>1399</span>
+              <dl>1699</dl>
+           </a>
           </li>
           <li>
-            <img src="https://i1.mifile.cn/a1/pms_1496997930.13622399!220x220.jpg" alt="">
-            <strong></strong>
-            <p></p>
-            <span></span>
+            <a href="https://www.mi.com/mitv4A/43y/">
+              <img src="https://i1.mifile.cn/a1/pms_1500287084.72131750!220x220.jpg" alt="">
+              <strong> 小米电视4A 43英寸青春版3</strong>
+              <p>全高清屏/人工智能语音</p>
+              <span>1399</span>
+              <dl>1699</dl>
+           </a>
           </li>
           <li>
-            <img src="https://i1.mifile.cn/a1/pms_1505897517.20426731!220x220.jpg" alt="">
-            <strong></strong>
-            <p></p>
-            <span></span>
-           </li>
-           <li>
-              <img src="https://i1.mifile.cn/a4/xmad_14972549116226_tZpod.png" alt="">
-              <strong></strong>
-              <p></p>
-             <span></span>
-            </li>
-            <li>
-              <img src="https://i1.mifile.cn/a1/pms_1504498936.11861982!220x220.jpg" alt="">
-              <strong></strong>
-              <p></p>
-              <span></span>
-            </li>
-            <li>
-              <img src="https://i1.mifile.cn/a4/xmad_15281923207128_rOfDp.png" alt="">
-              <strong></strong>
-              <p></p>
-              <span></span>
-            </li>
+            <a href="https://www.mi.com/mitv4A/43y/">
+              <img src="https://i1.mifile.cn/a1/pms_1490077569.08131612!220x220.png" alt="">
+              <strong> 小米电视4A 43英寸青春版4</strong>
+              <p>全高清屏/人工智能语音</p>
+              <span>1399</span>
+              <dl>1699</dl>
+           </a>
+          </li>
+          <li>
+            <a href="https://www.mi.com/mitv4A/43y/">
+              <img src="https://i1.mifile.cn/a1/pms_1509694019.50094281!220x220.png" alt="">
+              <strong> 小米电视4A 43英寸青春版5</strong>
+              <p>全高清屏/人工智能语音</p>
+              <span>1399</span>
+              <dl>1699</dl>
+           </a>
+          </li>
+          <li>
+            <a href="https://www.mi.com/mitv4A/43y/">
+              <img src="https://i1.mifile.cn/a1/pms_1499072633.96298268!220x220.jpg" alt="">
+              <strong> 小米电视4A 43英寸青春版6</strong>
+              <p>全高清屏/人工智能语音</p>
+              <span>1399</span>
+              <dl>1699</dl>
+           </a>
+          </li>
+          <li>
+            <a href="https://www.mi.com/mitv4A/43y/">
+              <img src="https://i1.mifile.cn/a1/pms_1524883847.49276938!220x220.jpg" alt="">
+              <strong> 小米电视4A 43英寸青春版7</strong>
+              <p>全高清屏/人工智能语音</p>
+              <span>1399</span>
+              <dl>1699</dl>
+           </a>
+          </li>
             <li>
               <div class="ads-right--1">
                 <img src="https://i1.mifile.cn/a1/pms_1521634907.16181074!220x220.jpg" alt="">
@@ -82,6 +101,7 @@
 .container
   width 1226px
   margin 0 auto
+  background-color #fff
   .hts
     font-size 22px
     // margin 0
@@ -91,7 +111,7 @@
     // float left
     // width 100%
   .ads
-    width 1200
+    width 1226
     margin 4px 2px
     .ads-left
       float left
@@ -104,8 +124,26 @@
       li
         float left
         margin-left 14px
-      img
         width 234px
         height 246px
-        float left
+        text-align center
+        padding 34px 0 20px
+        strong
+          display block
+          color black
+        p
+          display block
+          color #b0b0b0
+          font-size 12px
+        span
+          color red
+          font-size 14px
+        dl
+          display inline
+          text-decoration line-through
+          color #b0b0b0
+          font-size 14px
+      img
+        width 150px
+        height 150px
 </style>
