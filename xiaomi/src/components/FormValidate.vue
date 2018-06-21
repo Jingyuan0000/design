852fe7b2997e8f66268d9a6b7c8c5d0b8055b643
Jill <18687018392@163.com>
2018-06-21 18:32:17 +0800

    topbar完成

---
+++ xiaomi/src/components/FormValidate.vue
@@ -0,0 +1,66 @@
+<template>
+  <form id="app" @submit="checkForm" action="https://vuejs.org/" method="post">
+    <Radio v-model="single">Radio</Radio>
+    <p v-if="errors.length">
+      <b>Please correct the following error(s):</b>
+      <ul>
+        <li v-for="(error, idx) in errors" :key="idx">{{ error }}</li>
+      </ul>
+    </p>
+
+    <p>
+      <label for="name">Name</label>
+      <input type="text" name="name" id="name" v-model="name">
+    </p>
+
+    <p>
+      <label for="age">Age</label>
+      <input type="number" name="age" id="age" v-model="age" min="0">
+    </p>
+
+    <p>
+      <label for="movie">Favorite Movie</label>
+      <select name="movie" id="movie" v-model="movie">
+        <option>Star Wars</option>
+        <option>Vanilla Sky</option>
+        <option>Atomic Blonde</option>
+      </select>
+    </p>
+
+    <p>
+      <input type="submit" value="Submit">
+    </p>
+
+  </form>
+</template>
+<script>
+import { Radio } from 'iview'
+export default {
+  el: '#app',
+  data () {
+    return {
+      errors: [],
+      name: null,
+      age: null,
+      movie: null,
+      singal: false
+    }
+  },
+  methods: {
+    checkForm: function (e) {
+      if (this.name && this.age) return true
+      this.errors = []
+      if (!this.name) this.errors.push('Name required.;')
+      if (!this.age) this.errors.push('Age required.')
+      e.preventDefault()
+    }
+  }
+}
+</script>
+
+<style>
+input,
+select {
+  margin-left: 10px;
+}
+</style>
--- xiaomi/src/components/Part1.vue
+++ xiaomi/src/components/Part1.vue
@@ -1,46 +1,69 @@
 <template>
-  <div class="top">
-    <div class="top-left">
-      <a href="https://www.mi.com/index.html">小米商城</a>
-      <span class="seprate">|</span>
-      <a href="https://www.miui.com/">MIUI</a>
-      <span class="seprate">|</span>
-      <a href="https://iot.mi.com/index.html">loT</a>
-      <span class="seprate">|</span>
-      <a href="https://i.mi.com">云服务</a>
-      <span class="seprate">|</span>
-      <a href="https://jr.mi.com/index.html?from=micom&t=1529569073565">金融</a>
-      <span class="seprate">|</span>
-      <a href="https://youpin.mi.com/">有品</a>
-      <span class="seprate">|</span>
-      <a href="https://xiaoai.mi.com/">小爱开放平台</a>
-      <span class="seprate">|</span>
-      <a href="https://b.mi.com/?client_id=180100031058&masid=17409.0358">政企服务</a>
-      <span class="seprate">|</span>
-      <a href="">Select Region</a>
-    </div>
-    <div class="top-right">
-      <a href="">登陆</a>
-      <span class="seprate">|</span>
-      <a href="">注册</a>
-      <span class="seprate">|</span>
-      <a href="">消息通知</a>
-      <a href="">
-        <img src="" alt="">购物车</a>
+  <div class="topbar">
+    <div class="topbar_main r">
+      <div class="topbar_left">
+        <a href="https://www.mi.com/index.html">小米商城</a>
+        <span class="separate">|</span>
+        <a href="https://www.miui.com/">MIUI</a>
+        <span class="separate">|</span>
+        <a href="https://iot.mi.com/index.html">loT</a>
+        <span class="separate">|</span>
+        <a href="https://i.mi.com">云服务</a>
+        <span class="separate">|</span>
+        <a href="https://jr.mi.com/index.html?from=micom&t=1529569073565">金融</a>
+        <span class="separate">|</span>
+        <a href="https://youpin.mi.com/">有品</a>
+        <span class="separate">|</span>
+        <a href="https://xiaoai.mi.com/">小爱开放平台</a>
+        <span class="separate">|</span>
+        <a href="https://b.mi.com/?client_id=180100031058&masid=17409.0358">政企服务</a>
+        <span class="separate">|</span>
+        <a href="">Select Region</a>
+      </div>
+      <div class="topbar_right">
+        <a href="">登陆</a>
+        <span class="separate">|</span>
+        <a href="">注册</a>
+        <span class="separate">|</span>
+        <a href="">消息通知</a>
+        <span class="separate">|</span>
+        <div class="topbar--cart">
+          <a href="">
+            <img src="https://static.mi.com/cart/" alt="">购物车</a>
+
+        </div>
+
+      </div>
     </div>
 
   </div>
 </template>
-<style lang="stylus" scoped>
-.top
+<style lang="stylus">
+.wrap
+  width 1226px
+.topbar
   display block
   position relative
   height 40px
   background #333
   color #b0b0b0
-  font-size 12px
-  .top-left
+  // font-size 14px
+  font 12px / 1.5 'Helvetica Neue', Helvetica, Arial, 'Microsoft Yahei', 'Hiragino Sans GB', 'Heiti SC', 'WenQuanYi Micro Hei', sans-serif
+  line-height 40px
+  .topbar_main, a
+    color #b0b0b0
+  .topbar_left
     float left
-  .top-right
+    display inline
+  .topbar_right
     float right
+    display inline
+  .topbar--cart
+    float right
+    background #424242
+    text-align center
+    width 120px
+  .separate
+    margin 0 0.5em
+    color #424242
 </style>
