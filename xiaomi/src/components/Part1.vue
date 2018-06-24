376be30e697443144335dba849d296262028c6ef
Jill <jill.li.0822@gmail.com>
2018-06-24 23:25:20 +0800

    main-menu-1

--- xiaomi/src/components/Part1.vue
+++ xiaomi/src/components/Part1.vue
@@ -98,11 +98,103 @@
         </div>
         <div class="navbar_right">
           <input type="search" class="item-search">
-          <div class="hotwords">
-            <a href="">小米8</a>
-            <a href="">小米MIX 2S</a>
+          <div class="search-btn">
+            <a href=""><img src="../../static/image/logo/search.png" alt=""></a>
           </div>
-          <a href=""><img src="../../static/image/logo/search.png" alt=""></a>
+          <div class="hotsearch">
+              <a href="https://www.mi.com/mi8/">小米8</a>
+              <a href="https://item.mi.com/product/10000085.html">小米MIX 2S</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="home-category">
+      <div class="main-menu">
+
+          <div class="main-menu-left">
+            <ul class="menu-list">
+              <li class="title">
+                <a href="">手机 电话卡</a>
+              </li>
+              <li class="title">
+                <a href="">电视 盒子</a>
+              </li>
+              <li class="title">
+                <a href="">笔记本</a>
+              </li>
+              <li class="title">
+                <a href="">智能 家电</a>
+              </li>
+              <li class="title">
+                <a href="">健康 家居</a>
+              </li>
+              <li class="title">
+                <a href="">出行 儿童</a>
+              </li>
+              <li class="title">
+                <a href="">路由器 手机配件</a>
+              </li>
+              <li class="title">
+                <a href="">移动电源 插线板</a>
+              </li>
+              <li class="title">
+                <a href="">耳机 音响</a>
+              </li>
+              <li class="title">
+                <a href="">生活 米兔</a>
+              </li>
+            </ul>
+          </div>
+        <div class="main-menu-right">
+          <a href="">
+            <img src="../../static/image/banner/main-menu-right.jpg" alt="">
+          </a>
+        </div>
+
+      </div>
+      <div class="sep-row">
+        <div class="channel-span">
+          <ul class="channel-list clearfix">
+            <li>
+              <a href=""><img src="../../static/image/icon/icon1.png" alt=""></a>
+              <i>选购手机</i>
+            </li>
+              <li>
+              <a href=""><img src="../../static/image/icon/icon2.png" alt=""></a>
+            </li>
+              <li>
+              <a href=""><img src="../../static/image/icon/icon3.png" alt=""></a>
+            </li>
+              <li>
+              <a href=""><img src="../../static/image/icon/icon4.png" alt=""></a>
+            </li>
+              <li>
+              <a href=""><img src="../../static/image/icon/icon5.png" alt=""></a>
+            </li>
+              <li>
+              <a href=""><img src="../../static/image/icon/icon6.png" alt=""></a>
+            </li>
+
+          </ul>
+        </div>
+        <div class="min-banner">
+          <ul class="home-pro clearfix">
+            <li class="first">
+              <a href="">
+                <img src="../../static/image/banner/min-banner1.jpg" alt="">
+              </a>
+            </li>
+            <li class="second">
+              <a href="">
+                <img src="../../static/image/banner/min-banner2.jpg" alt="">
+              </a>
+            </li>
+            <li class="third">
+              <a href="">
+                <img src="../../static/image/banner/min-banner3.jpg" alt="">
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
@@ -116,19 +208,18 @@
   display block
   position relative
   height 40px
+  width 1226px
   background #333
   color #b0b0b0
   // font-size 14px
   font 12px / 1.5 'Helvetica Neue', Helvetica, Arial, 'Microsoft Yahei', 'Hiragino Sans GB', 'Heiti SC', 'WenQuanYi Micro Hei', sans-serif
   line-height 40px
-  .topbar_main, a
+  .topbar_main,a
     color #b0b0b0
   .topbar_left
     float left
-    display inline
   .topbar_right
     float right
-    display inline
   .topbar--cart
     float right
     background #424242
@@ -140,6 +231,7 @@
 .navbar
   // display block
   .navbar_main
+    margin 0
     height 100px
     .navbar_left
       float left
@@ -147,30 +239,27 @@
       height 55px
       margin-top 22px
       margin-right 10px
-      background red
     .navbar_center
       width 820px
       height 88px
       float left
-      background blue
       padding 12px 0 0 30px
-      a
-        display block
-        padding 26px 10px 38px
-        color #333
-        font-size 16px
-        float left
       ul.navlist
         float left
         li
           float left
           list-style none
+          a
+            display block
+            padding 26px 10px 38px
+            color #333
+            font-size 16px
+            float left
     .navbar_right
       float right
       width 296px
       height 50px
       margin-top 25px
-      background orange
       .item-search
         float left
         width 245px
@@ -178,34 +267,120 @@
         padding 0 10px
         border 1px solid #e0e0e0
         position relative
-        .hotwords
-          position absolute
-          top 14px
-          right 100px
-          z-index 2
-          text-align right
-          width 124.05px
-          height 21px
-          a
-            // display inline-block
-            float left
-            color #757575
-            font-size 12px
-            margin-left 5px
-            padding 0 5px
-            background #eee
+      .search-btn
+        position absolute
+        top 24px
+        left 1173px
+        z-index 2
+        width 52px
+        height 50px
+        border 1px solid #e0e0e0
+        font-size 24px
+        line-height 24px
+        color #616161
+        outline 0
+      .hotsearch
+        position absolute
+        left 1060px
+        top 40px
+        z-index 2
+        text-align right
+        width 124.05px
+        height 21px
+        a
+          background #eee
+          display inline-block
+          float left
+          color #757575
+          font-size 12px
+          margin-left 5px
+          padding 0 5px
             // font 14px / 1.5 'Helvetica Neue', Helvetica, Arial, 'Microsoft Yahei', 'Hiragino Sans GB', 'Heiti SC', 'WenQuanYi Micro Hei', sans-serif
-        .search-btn
-          position absolute
-          right 0
-          top 0
-          z-index 2
-          width 52px
-          height 50px
-          border 1px solid #e0e0e0
-          font-size 24px
-          line-height 24px
-          background #fff
-          color #616161
-          outline 0
+.home-category
+  width 1226px
+  height 644px
+  background orange
+  .main-menu
+    position relative
+    height 460px
+    ul.category
+      width 234px
+      height 460px
+  .main-menu-left
+    display block
+    position absolute
+    float left
+    top 0
+    left 0
+    z-index 21
+    width 234px
+    height 460px
+    font-size 14px
+    position: relative;
+    overflow: hidden;
+    ul.menu-list
+      height: 460px;
+      border: 0;
+      color: #fff;
+      background: #333;
+      background: rgba(0,0,0,0.6);
+      li.title, a
+        position: relative;
+        display: block;
+        padding-left: 30px;
+        height: 42px;
+        line-height: 42px;
+        color: #fff
+        display: list-item;
+        text-align: -webkit-match-parent;
+        font-size 14px
+  .main-menu-right
+    img
+      width 992px
+      height 460px
+      display block
+      float left
+  .sep-row
+    width 1240px
+    height 170px
+    margin-top 14px
+  .channel-span
+    width 234px
+    height 170px
+    float left
+    // margin-left 14px
+    min-height 1px
+    ul.channel-list
+      margin: 0;
+      padding: 3px;
+      list-style-type: none;
+      font-size: 12px;
+      text-align: center;
+      background: #5f5750
+      li
+        float left
+  .min-banner
+    width 978px
+    height 170px
+    float: left;
+    min-height: 1px;
+  ul.home-pro
+    list-style-type: none
+    width 978px
+    li.first
+      float: left;
+      width: 316px;
+      height: 170px;
+      margin-left 15px
+    li.second
+      float: left;
+      width: 316px;
+      height: 170px;
+      margin-left 15px
+    li.third
+      float: left;
+      width: 316px;
+      height: 170px;
+      margin-left 15px
+
 </style>
